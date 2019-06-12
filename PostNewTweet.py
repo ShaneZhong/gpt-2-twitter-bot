9bc3cf9e4f4654d2a9cda212715afbf8e6f0e5e0
ShaneZhong <shane.zhong@gmail.com>
2019-06-12 11:11:28 +1000

    Create a python script to post a tweet
    The following environment variable is required to make the script work:
        consumer_key = os.environ['consumer_key']
        consumer_secret = os.environ['consumer_secret']
        access_token = os.environ['access_token']
        access_token_secret = os.environ['access_token_secret']

---
+++ PostNewTweet.py
@@ -0,0 +1,62 @@
+import tweepy
+import os
+import pandas as pd
+
+# User Input:
+csv_dir = "./sample/test_output_tweet.csv"
+
+# Fetch environment variable
+try:
+    consumer_key = os.environ['consumer_key']
+    consumer_secret = os.environ['consumer_secret']
+    access_token = os.environ['access_token']
+    access_token_secret = os.environ['access_token_secret']
+
+    print(f'Current working directory: {os.getcwd()}')
+    print("=" * 30)
+    print("Twitter API Credential:")
+    print(consumer_key[:5])
+    print(consumer_secret[:5])
+    print(access_token[:5])
+    print(access_token_secret[:5])
+    print("=" * 30)
+except:
+  print("ERROR: No environment variables found.")
+  print("Please add consumer_key, consumer_secret,access_token and access_token_secret.")
+
+
+# Code to access the account
+try:
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+
+    user = api.me()
+    print("This is my user name: "+user.name)
+except:
+    print("ERROR: Twitter API call failed. Please check your twitter API Access code.")
+
+# Select the first line from the output_tweet.csv file
+try:
+    df_raw = pd.read_csv(csv_dir)
+
+    selected_tweet = df_raw.output_tweet[0]
+    print(selected_tweet)
+except:
+    print(f"ERROR: No csv file found or the file does not contain any tweet! The current dir is: {csv_dir}")
+
+# post the tweet
+try:
+    api.update_status(selected_tweet)
+    print("New Tweet Posted Successfully!")
+except:
+    print("ERROR: Failed to post tweet.")
+
+try:
+    # exclude the first row 0 from the data frame
+    df = df_raw[1:].copy()
+    df
+    df.to_csv(csv_dir, index=False, header=True)
+    print("CSV file updated.")
+except:
+    print(f"ERROR: Failed to update the csv file. The current dir is: {csv_dir}")
